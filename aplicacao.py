ecf05d24adeab54b0c15673a83ca1e4f448d41c6
JBVJoaoV <joaopedrodesousavieira@gmail.com>
2023-11-20 19:22:03 -0300

    menu funcional

--- aplicacao.py
+++ aplicacao.py
@@ -1,10 +1,43 @@
-#def cadastro ():
+import json
 
-#def login ():
+def cadastro():
+    """Para cadastrar novos usuarios"""
+    nome = input("Digite seu nome: ")
+    idade = int(input("Digite sua idade: "))
+    cpf = int(input("Digite seu CPF (somente numeros): "))
+    altura = float(input("Digite sua altura (em metros, exemplo: 1.8): "))
+    peso = float(input("Digite seu peso (em quilogramas): "))
+    tempo_atividade = int(input("Digite o tempo de atividade física por semana (em minutos): "))
+    tempo_sono = int(input("Digite o tempo de sono por dia: "))
 
-#def dados ():
+    usuario = {
+        'nome': nome,
+        'idade': idade,
+        'cpf': cpf,
+        'altura': altura,
+        'peso': peso,
+        'tempo_atividade': tempo_atividade,
+        'tempo_sono': tempo_sono
+    }
+
+    try:
+        with open('acesso.txt', 'r') as arquivo:
+            conteudo_existente = arquivo.read()
+    except FileNotFoundError:
+        conteudo_existente = ""
+
+    with open('acesso.txt', 'w') as arquivo:
+        dados_existentes = json.loads(conteudo_existente) if conteudo_existente else []
+
+        dados_existentes.append(usuario)
+
+        arquivo.write(json.dumps(dados_existentes, indent=2))
+
+def dados ():
+    """Exibe os dados do usuario"""
 
 def IMC (altura, peso):
+    """Calcula o IMC"""
     imc = peso / (altura**2)
     
     if imc <= 16 :
@@ -25,37 +58,31 @@ def IMC (altura, peso):
     else:
         print(f'Obesidade morbida, seu IMC é: {imc}')
 
-#def sedentarismo ():
+def sedentarismo ():
+    """Calcula o nivel de sedentarismo"""
 
+def sair():
+    return False
 
-x = 1 # Variavel para loop do menu
+while True:
+    print ("Escolha uma opção:\n 1 - Cadastro\n 2 - Exibir dados\n 3 - Calcular IMC\n 4 - Calcular nivel de sedentarismo\n 5 - Sair")
+    opcao = int(input("Opção: "))
 
-while x == 1:
-    print ("Escolha uma opção:")
-    print ("1 - Cadastro")
-    print ("2 - Login")
-    print ("3 - Exibir dados")
-    print ("4 - Calcular IMC")
-    print ("5 - Calcular nivel de sedentarismo")
-
-
-"""
-    match 6:
-        case 1:
+    match opcao:
+        case 1: 
             cadastro()
 
         case 2:
-            login() 
-
-        case 3:
             dados()
 
-        case 4:
+        case 3:
             IMC()
 
-        case 5:
+        case 4:
             sedentarismo()
 
-        case 6:
-            
-"""
+        case 5:
+            sair()
+        
+        case _:
+            print("Opção inválida.")
--- teste.py
+++ teste.py
@@ -1,1 +1,61 @@
-    
+import json
+
+def cadastro():
+    # Solicitando informações ao usuário
+    nome = input("Digite seu nome: ")
+    idade = int(input("Digite sua idade: "))
+    cpf = int(input("Digite seu CPF (somente numeros): "))
+    altura = float(input("Digite sua altura (em metros): "))
+    peso = float(input("Digite seu peso (em quilogramas): "))
+    tempo_atividade = int(input("Digite o tempo de atividade física por semana (em minutos): "))
+    tempo_sono = int(input("Digite o tempo de sono por dia: "))
+
+    # Criando um dicionário com as informações
+    usuario = {
+        'nome': nome,
+        'idade': idade,
+        'cpf': cpf,
+        'altura': altura,
+        'peso': peso,
+        'tempo_atividade': tempo_atividade,
+        'tempo_sono': tempo_sono
+    }
+
+    # Lendo o arquivo existente
+    try:
+        with open('acesso.txt', 'r') as arquivo:
+            conteudo_existente = arquivo.read()
+    except FileNotFoundError:
+        conteudo_existente = ""
+
+    # Adicionando o novo cadastro ao arquivo
+    with open('acesso.txt', 'w') as arquivo:
+        # Carregando dados existentes como uma lista de dicionários
+        dados_existentes = json.loads(conteudo_existente) if conteudo_existente else []
+
+        # Adicionando o novo usuário à lista
+        dados_existentes.append(usuario)
+
+        # Escrevendo a lista de dicionários formatada como JSON no arquivo
+        arquivo.write(json.dumps(dados_existentes, indent=2))
+
+def calcular_imc():
+    altura = usuario['altura']
+    peso = usuario['peso']
+    imc = peso / (altura ** 2)
+
+    if imc <= 16:
+        print(f'Desnutrição, seu IMC é: {imc}')
+    elif 16.1 <= imc <= 18.4:
+        print(f'Magreza, seu IMC é: {imc}')
+    elif 18.5 <= imc <= 24.9:
+        print(f'Peso normal para a altura, seu IMC é: {imc}')
+    elif 25 <= imc <= 29.9:
+        print(f'Sobrepeso, seu IMC é: {imc}')
+    elif 30 <= imc <= 39.9:
+        print(f'Obesidade, seu IMC é: {imc}')
+    else:
+        print(f'Obesidade mórbida, seu IMC é: {imc}')
+
+calcular_imc()
+
